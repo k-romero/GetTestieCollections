c65db91a05dbdbfb0d51258595c4cec23b5ebc9f
k-romero <imkevinromero@gmail.com>
2020-03-01 20:22:17 -0500

    create stack tests

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -1,17 +1,30 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 import java.util.Stack;
 
 public class TestStack {
+    Stack<String> stack;
+
 
     @org.junit.Before
     public void setUp() throws Exception {
+        stack = new Stack<>();
+        stack.push("First In");
+        stack.push("Second In");
+        stack.push("Third In");
+        stack.push("Fourth In");
+
     }
 
     @org.junit.After
     public void tearDown() throws Exception {
     }
+
+    //push,pop,peek,search,empty
 
     @org.junit.Test
     public void TestStack1() {
@@ -21,5 +34,32 @@ public class TestStack {
     }
 
 
-    // Make a bigger test exercising more Stack methods.....
+
+    @Test
+    public void popTest(){
+        Assert.assertEquals("Fourth In",stack.pop());
+    }
+
+    @Test
+    public void peekTest(){
+        Assert.assertEquals("Fourth In",stack.peek());
+    }
+
+    @Test
+    public void searchTest(){
+        Assert.assertEquals(4,stack.search("First In"));
+    }
+
+    @Test
+    public void emptyTest(){
+        stack.clear();
+        Assert.assertTrue(stack.isEmpty());
+    }
+
+    
+
+
+
+
+
 }
